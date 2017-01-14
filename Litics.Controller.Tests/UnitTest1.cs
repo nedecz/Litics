355d82a8cc4380326a54f1dec3ce67487e390486
nedecz <t.nedeczki@gmail.com>
2017-01-14 13:06:20 +0100

    Add the project template

---
+++ Litics.Controller.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Litics.Controller.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
